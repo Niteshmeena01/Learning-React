9be3c4a0ed664ffc5294048b5152bcccdf39a64c
Niteshmeena01 <niteshnm123456@gmail.com>
2024-01-19 15:07:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <h1>Hello</h1>
+     <h2>ok its running</h2>
+     <h3>GOOd</h3>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/Robot.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Robot = () => {
+  return (
+    <>
+      <h1 >new robo file</h1>  
+    </>
+  )
+}
+
+export default Robot
