b28c1ce9a039875f02b6b5678a38558322162afe
Niteshmeena01 <niteshnm123456@gmail.com>
2024-01-24 18:19:28 +0530

    newData

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from "react";
-import Robot from "./Robot";
-import Counter from "./test";
-import Time from "./time";
-import Add, { substract, Multiplaction, Division } from "./Add";
-import "./index.css";
+import React, { useState } from "react";
+import './index.css' ;
+
+
 
 const App = () => {
-  return (
-    <>
-      {/* <Counter/> */}
-      {/* <Time/> */}
-      {/* <Robot/> */}
-      {/* <ul className='List'>
-      <li>The numbers is add {Add(40,4)}</li>
-      <li>The number is substract {substract(30,3)}</li>
-      <li>The number is Multiply {Multiplaction(10,3)}</li>
-      <li>The number is Divide {Division(10,3)}</li>
-    </ul> */}
-    </>
-  );
+
+  let [input,inState] = useState("Hello")
+  const [FullName ,fState] = useState("")
+
+  const inputEvent = (event)=>{
+    inState(event.target.value)
+
+  }
+
+const inputValue = () => {
+        fState(input)
+        
+}
+
+ return(
+  <>
+  <div className="inputDiv">
+  <div className="centerDiv">
+  <center>
+    <h1>Sign up {FullName}</h1>
+    <br/>
+    <input type="text" placeholder="Enter Your Name" onChange={inputEvent}  style={{width:"300px",height:"30px"}}/>
+    <br/>
+    <br/>
+    <input type="Password" placeholder="Enter Your Password" onChange={inputEvent} style={{width:"300px",height:"30px"}}/>
+    <br/>
+    <br/>
+    <button onClick={inputValue} style={{width:"150px",height:"30px"}}>click me</button>
+    </center>
+    </div>
+    </div>
+  </>
+ )
 };
 
 export default App;
